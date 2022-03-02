39842d96c545d67f0882f0b6d9836549a1e9f922
Benny <conquera99@gmail.com>
2022-03-03 00:38:51 +0800

    update public api

--- src/pages/api/gallery/album.tsx
+++ src/pages/api/gallery/album.tsx
@@ -1,25 +1,20 @@
-import { getSession } from 'next-auth/react';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { prisma } from 'db';
 import { DEFAULT_LIMIT, forbiddenResponse, successResponse } from 'utils/constant';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-	const { p, s } = req.query;
+    const { p, s } = req.query;
 
-	const session = await getSession({ req });
+    //for list
+    const page = Number(p || 1);
+    const limit = Number(s || DEFAULT_LIMIT);
 
-	if (!session) return res.json(forbiddenResponse);
+    const data = await prisma.albums.findMany({
+        skip: (Number(page) - 1) * Number(limit),
+        take: Number(limit),
+        orderBy: { createdAt: 'desc' },
+    });
 
-	//for list
-	const page = Number(p || 1);
-	const limit = Number(s || DEFAULT_LIMIT);
-
-	const data = await prisma.albums.findMany({
-		skip: (Number(page) - 1) * Number(limit),
-		take: Number(limit),
-		orderBy: { createdAt: 'desc' },
-	});
-
-	res.json({ ...successResponse, data });
+    res.json({ ...successResponse, data });
 }
