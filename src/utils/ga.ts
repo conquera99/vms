ba1503fe183eab72313d94723605253a0b0cd672
Benny <conquera99@gmail.com>
2022-03-03 18:02:51 +0800

    add google analytics

---
+++ src/utils/ga.ts
@@ -0,0 +1,14 @@
+// log the pageview with their URL
+export const pageview = (url: string) => {
+	// @ts-ignore
+	window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+		page_path: url,
+	});
+};
+
+// log specific events happening.
+// @ts-ignore
+export const event = ({ action, params }) => {
+	// @ts-ignore
+	window.gtag('event', action, params);
+};
